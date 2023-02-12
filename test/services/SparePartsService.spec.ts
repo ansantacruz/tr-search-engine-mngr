781669c8c23f156df81f1d36af31320c734be5fa
Andres camilo Santacruz Borda <84866533+asantac@users.noreply.github.com>
2023-02-12 16:35:06 -0500

    TC-22: I have fixed unit test

--- test/services/SparePartsService.spec.ts
+++ test/services/SparePartsService.spec.ts
@@ -6,6 +6,7 @@ import SparePartsDataSource from '../../src/datasource/SparePartsDataSource';
 import { IError } from '../../src/model/IError';
 import { ISearchConfig } from '../../src/model/ISearchConfig';
 import { SparePartsService } from '../../src/services/SparePartsService';
+import { IProduct } from '../../src/model/IProduct';
 
 
 
@@ -18,6 +19,12 @@ const RESOLVE_RESPONSE = [{
    "marca": "Bogota",
    "estado": 1
 } ] as ISearchConfig [];
+
+const RESOLVE_RESPONSE_PRODUCT = [{
+   "productId": 1,
+   "productDescription": "Bogota",
+   "logo": "1"
+} ] as IProduct [];
 
 const REJECT_RESPONSE = {
    "EndDt": "01/01/2023",
@@ -80,6 +87,30 @@ describe('SparePartsService', () => {
       });
 
       SparePartsService.getMotorcyclebyBrand(1)
+      .catch((err) => {
+         expect(err.Status.StatusCode).equal(404);
+      });
+   });
+
+   it('getSapareParts getSearchConfig resolve', () => {
+
+      sinon.replace(SparePartsDataSource, 'getSapareParts', (): Promise<IProduct[]> => {
+      return Promise.resolve(RESOLVE_RESPONSE_PRODUCT);
+      });
+
+      SparePartsService.getSapareParts(1,1)
+      .then((res) => {
+         expect(res[0].productDescription).equal('Bogota');
+      });
+   });
+
+   it('SparePartsService getSapareParts reject', () => {
+
+      sinon.replace(SparePartsDataSource, 'getSapareParts', (): Promise<any> => {
+      return Promise.reject(REJECT_RESPONSE);
+      });
+
+      SparePartsService.getSapareParts(1,1)
       .catch((err) => {
          expect(err.Status.StatusCode).equal(404);
       });
